8b5a073aa87b860093b0294aca2e756d30d584df
Michael Lytzen Hansen <mlh@terma.com>
2024-03-17 01:50:08 +0100

    Added keymappings for easy movement in terminal mode
    However, it doesn't really work. I have stolen it from a previous configuration,
    and I thought it worked there.

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,3 +1,13 @@
+-- This function for setting the good movements, does not seem to work.
+-- Not sure why. I have stolen it from my previous configuration.
+function _G.set_terminal_keymaps()
+  -- vim.print 'set_terminal_keymaps called'
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-_><C-n><C-W>h]], { noremap = true })
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-_><C-n><C-W>j]], { noremap = true })
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-_><C-n><C-W>k]], { noremap = true })
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-_><C-n><C-W>l]], { noremap = true })
+end
+
 return {
   'akinsho/toggleterm.nvim',
   version = '*',
@@ -27,5 +37,16 @@ return {
       local lazygit = Terminal:new { cmd = 'lazygit', hidden = true }
       lazygit:toggle()
     end, { noremap = true, silent = true, desc = 'Toggle Lazygit' }),
+
+    -- The usual move keys.
+    -- vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()',
+    vim.api.nvim_create_autocmd('TermOpen', {
+      desc = 'Make moving between windows easier',
+      pattern = 'term://*',
+      group = vim.api.nvim_create_augroup('terminal_keymaps', { clear = true }),
+      callback = function()
+        set_terminal_keymaps()
+      end,
+    }),
   },
 }
