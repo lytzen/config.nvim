6174bae4eeb02f576568c35062c7abbc3a2ab9c9
Michael Lytzen Hansen <mlh@terma.com>
2024-12-29 23:05:57 +0100

    Cleaned up

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -9,7 +9,7 @@
 return {
   -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
-  lazy = true,
+  -- lazy = true,
   -- NOTE: And you can specify dependencies as well
   dependencies = {
     -- Creates a beautiful debugger UI
@@ -22,7 +22,6 @@ return {
 
     -- Add your own debuggers here
     'mfussenegger/nvim-dap-python',
-    'leoluz/nvim-dap-go',
     'nvim-neotest/nvim-nio',
   },
   config = function()
@@ -42,45 +41,46 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
       },
     }
 
     -- Basic debugging keymaps, feel free to change to your liking!
     vim.keymap.set('n', '<leader>Dc', dap.continue, { desc = '[D]ebug: Start/[C]ontinue' })
-    vim.keymap.set('n', '<leader>DR', dap.run_to_cursor, { desc = '[D]ebug: Start/[C]ontinue' })
-    vim.keymap.set('n', '<leader>Di', dap.step_into, { desc = 'Debug: Step Into' })
-    vim.keymap.set('n', '<leader>Do', dap.step_over, { desc = 'Debug: Step Over' })
-    vim.keymap.set('n', '<leader>Du', dap.step_out, { desc = 'Debug: Step Out' })
+    vim.keymap.set('n', '<leader>DR', dap.run_to_cursor, { desc = '[D]ebug: [R]un to cursor' })
+    vim.keymap.set('n', '<leader>Di', dap.step_into, { desc = '[D]ebug: Step [i]nto' })
+    vim.keymap.set('n', '<leader>Do', dap.step_over, { desc = '[D]ebug: Step [o]ver' })
+    vim.keymap.set('n', '<leader>Du', dap.step_out, { desc = '[D]ebug: Step O[u]t' })
     vim.keymap.set('n', '<leader>Db', dap.toggle_breakpoint, { desc = '[D]ebug: Toggle [B]reakpoint' })
     vim.keymap.set('n', '<leader>DB', function()
       dap.set_breakpoint(vim.fn.input '[Condition] > ')
-    end, { desc = '[D]ebug: Set [B]reakpoint' })
+    end, { desc = '[D]ebug: Set Conditional [B]reakpoint' })
 
     vim.keymap.set('n', '<leader>Dq', dap.close, { desc = '[D]ebug: [Q]uit' })
-    vim.keymap.set('n', '<leader>Dx', dap.terminate, { desc = '[D]ebug: [T]erminate' })
+    vim.keymap.set('n', '<leader>Dx', dap.terminate, { desc = '[D]ebug: e[x]it' })
 
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
-    dapui.setup {
-      -- Set icons to characters that are more likely to work in every terminal.
-      --    Feel fr
-      --    Don't feel like these are good choices.
-      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-      controls = {
-        icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
-        },
-      },
-    }
+    dapui.setup()
+    -- dapui.setup {
+    --   -- Set icons to characters that are more likely to work in every terminal.
+    --   --    Feel fr
+    --   --    Don't feel like these are good choices.
+    --   icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+    --   controls = {
+    --     enabled = true,
+    --     icons = {
+    --       pause = '⏸',
+    --       play = '▶',
+    --       step_into = '⏎',
+    --       step_over = '⏭',
+    --       step_out = '⏮',
+    --       step_back = 'b',
+    --       run_last = '▶▶',
+    --       terminate = '⏹',
+    --       disconnect = '⏏',
+    --     },
+    --   },
+    -- }
 
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
     vim.keymap.set('n', '<leader>DU', dapui.toggle, { desc = '[D]ebug: [U]I See last session result.' })
@@ -100,6 +100,32 @@ return {
     -- debugpy is installed in the pyenv "debugpy", hence the
     -- reference to python in the pyenv shims.
     -- (The installation instructions recommended this approach)
+    dap.adapters.bashdb = {
+      type = 'executable',
+      command = vim.fn.stdpath 'data' .. '/mason/packages/bash-debug-adapter/bash-debug-adapter',
+      name = 'bashdb',
+    }
+    dap.configurations.sh = {
+      {
+        type = 'bashdb',
+        request = 'launch',
+        name = 'Launch file',
+        showDebugOutput = true,
+        pathBashdb = vim.fn.stdpath 'data' .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb',
+        pathBashdbLib = vim.fn.stdpath 'data' .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir',
+        trace = true,
+        file = '${file}',
+        program = '${file}',
+        cwd = '${workspaceFolder}',
+        pathCat = 'cat',
+        pathBash = 'bash',
+        pathMkfifo = 'mkfifo',
+        pathPkill = 'pkill',
+        args = {},
+        env = {},
+        terminalKind = 'integrated',
+      },
+    }
     require('dap-python').setup '~/.pyenv/shims/python'
   end,
 }
