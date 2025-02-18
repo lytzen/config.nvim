78b7fd4db9d65e30a9284947b1ecc2ad90b91497
Michael Lytzen Hansen <mlh@terma.com>
2025-02-18 19:22:04 +0100

    Small lualine tweaks

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -6,7 +6,7 @@ return {
       options = {
         icons_enabled = true,
         theme = 'auto',
-        component_separators = { left = '', right = '' },
+        component_separators = '',
         section_separators = { left = '', right = '' },
         disabled_filetypes = {
           statusline = {},
@@ -16,9 +16,9 @@ return {
         always_divide_middle = true,
         globalstatus = false,
         refresh = {
-          statusline = 1000,
-          tabline = 1000,
-          winbar = 1000,
+          statusline = 500,
+          tabline = 500,
+          winbar = 500,
         },
       },
       sections = {
